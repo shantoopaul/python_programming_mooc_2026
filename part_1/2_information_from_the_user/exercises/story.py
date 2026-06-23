7d975dbe1842545ce5af30373067f1c22d18ef69
shantoopaul <shantoopaul@gmail.com>
2026-06-24 00:57:29 +0600

    complete: part 1 lesson 2

---
+++ part_1/2_information_from_the_user/exercises/story.py
@@ -0,0 +1,4 @@
+name = input("Please type in a name: ")
+birthyear = input("Please type in a year: ")
+
+print(name + " is a valiant knight, born in the year " + birthyear + ". One morning " + name + " woke up to an awful racket: a dragon was approaching the village. Only " + name + " could save the village's residents.")
---
+++ part_1/2_information_from_the_user/exercises/utterances.py
@@ -0,0 +1,4 @@
+part_one = input("The 1st part: ")
+part_two = input("The 2nd part: ")
+part_three = input("The 3rd part: ")
+print(part_one + "-" + part_two + "-" + part_three + "!")
