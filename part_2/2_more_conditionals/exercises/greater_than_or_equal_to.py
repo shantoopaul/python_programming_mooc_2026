8dd039bde07deff2713d95b0be3fcedaea41081c
shantoopaul <shantoopaul@gmail.com>
2026-06-25 00:40:59 +0600

    complete: part 2 lesson 2

---
+++ part_2/2_more_conditionals/exercises/greater_than_or_equal_to.py
@@ -0,0 +1,9 @@
+num_1 = int(input("Please type in the first number: "))
+num_2 = int(input("Please type in another number: "))
+
+if num_1 > num_2:
+    print("The greater number was:", num_1)
+elif num_2 > num_1:
+    print("The greater number was:", num_2)
+else:
+    print("The numbers are equal!")
